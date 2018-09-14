7f7d90f7263b74044bbd85be4996eb7a7b6980aa
Kernael92 <kernaelapuko5@gmail.com>
2018-09-14 17:05:22 +0300

    modify the script.js file

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 var triangles = ["EQUILATERAL", "ISOSCELES", "SCALENE","NOT A TRIANGLE"]
    triangles.forEach(function(triangle)){
    alert("It is " + triangle + "!")
-   }
+};
 
-   var a = AB
-   var b = BC
-   var c = CA
+   var a = AB;
+   var b = BC;
+   var c = CA;
 
 if (AB===BC===CA)  {
-   alert("It is EQUILATERAL")
+   alert("It is EQUILATERAL");
 } else if (AB===BC!==CA && CA===BC!==AB && AB===CA!==BC){
-   alert("It is ISOSCELES")
+   alert("It is ISOSCELES");
 } else if (AB!==BC!==CA){
-   alert("It is SCALENE)"
+   alert("It is SCALENE");
 } else if(AB+BC<=CA || CA+AB<=BC || BC+CA<=AB){
-   alert("It is NOT A TRIANGLE")
+   alert("It is NOT A TRIANGLE");
 } else {
-   alert("Try again")
+   alert("Try again");
 }
