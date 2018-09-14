ab504e3ede3aced74008bf5697a262f5839dbe5a
Kernael92 <kernaelapuko5@gmail.com>
2018-09-14 16:14:10 +0300

    add the variables and conditions on the script.js file

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var triangles = ["EQUILATERAL", "ISOSCELES", "SCALENE","NOT A TRIANGLE"]
+   triangles.forEach(function(triangle)){
+   alert("It is " + triangle + "!")
+   }
+
+   var a = AB
+   var b = BC
+   var c = CA
+
+if (AB===BC===CA)  {
+   alert("It is EQUILATERAL")
+} else if (AB===BC!==CA && CA===BC!==AB && AB===CA!==BC){
+   alert("It is ISOSCELES")
+} else if (AB!==BC!==CA){
+   alert("It is SCALENE)"
+} else if(AB+BC<=CA || CA+AB<=BC || BC+CA<=AB){
+   alert("It is NOT A TRIANGLE")
+} else {
+   alert(Try again)
+}
