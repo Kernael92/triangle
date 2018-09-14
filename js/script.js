9cac10dc3713aa0a41bc8c5f7c3da273f0b55a74
Kernael92 <kernaelapuko5@gmail.com>
2018-09-14 18:19:46 +0300

    add the method .getElementById to the script.js file

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
-var triangles = ["EQUILATERAL", "ISOSCELES", "SCALENE","NOT A TRIANGLE"]
-   triangles.forEach(function(triangle)){
-   alert("It is " + triangle + "!")
-};
+Function triangles ()
+var a = document.getElementById("AB")
+var b = document.getElementById("BC")
+var c = document.getElementById("CA")
 
    var a = AB;
    var b = BC;
    var c = CA;
 
-if (AB===BC===CA)  {
+
+if (AB === BC === CA)  {
    alert("It is EQUILATERAL");
-} else if (AB===BC!==CA && CA===BC!==AB && AB===CA!==BC){
+} else if (AB === BC !== CA && CA === BC !== AB && AB === CA !== BC){
    alert("It is ISOSCELES");
-} else if (AB!==BC!==CA){
+} else if (AB !== BC !== CA){
    alert("It is SCALENE");
-} else if(AB+BC<=CA || CA+AB<=BC || BC+CA<=AB){
+} else if(AB + BC <= CA || CA + AB <= BC || BC + CA <= AB){
    alert("It is NOT A TRIANGLE");
 } else {
    alert("Try again");
